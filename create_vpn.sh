b0a5272aa7f871559b00be6149b08692ff02fb2c
Casey Marshall <cmars@cmarstech.com>
2015-08-08 20:23:05 -0500

    Remove DO automation, fix masquerade setup script.

--- create_vpn.sh
+++ create_vpn.sh
@@ -1,30 +1,42 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 echo "Script started at $(date)"
 
-if [ "$1" = "--nocreate" ]; then
-  shift
-else
-  ansible-playbook -K vpn_digital_ocean.yml $@
+VPN_HOST=$1
+if [ -z "$VPN_HOST" ]; then
+	echo "usage: $0 <vpn host>"
 fi
 
+VPN_REMOTE_USER=$2
+if [ -z "$VPN_REMOTE_USER" ]; then
+	VPN_REMOTE_USER=ubuntu
+fi
+
+_inv_file=$(mktemp)
+trap "rm -f $_inv_file" EXIT
+cat >>$_inv_file <<EOF
+[vpn]
+$VPN_HOST ansible_ssh_user=$VPN_REMOTE_USER
+
+EOF
+
 echo -n "Waiting for host to get up "
-while ! ping -c 1 -W 5 vpn > /dev/null; do
+while ! ping -c 1 -W 5 $VPN_HOST > /dev/null; do
   echo -n "."
   sleep 1 
 done
 echo
 
 echo -n "Waiting for SSH server on the host to get up "
-while ! netcat -z vpn 22; do
+while ! netcat -z $VPN_HOST 22; do
   echo -n "."
   sleep 1 
 done
 sleep 3
 echo
 
-ansible-playbook vpn.yml $@
-ansible-playbook vpn_teardown.yml $@
+ansible-playbook -i $_inv_file vpn.yml
+ansible-playbook -i $_inv_file vpn_teardown.yml
 
 which notify-send > /dev/null && notify-send -i call-start 'VPN gateway is online'
 
--- files/do_masquerade.sh
+++ files/do_masquerade.sh
@@ -4,6 +4,6 @@
 # Manual changes will be lost.
 
 # Don't add masquerading rule if it is already exists
-iptables -n -t nat -L POSTROUTING | grep -q MASQUERADE && exit 0 ||:
+iptables -n -t nat -L POSTROUTING | grep -q 'MASQUERADE.*10.8.0' && exit 0 ||:
 
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
